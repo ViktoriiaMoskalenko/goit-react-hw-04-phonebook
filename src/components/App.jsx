d077407d9b3bf92b260c56454b898bde4f15d311
Viktoriia Moskalenko <vika01mos@gmail.com>
2023-01-29 18:34:24 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { useState } from "react";
+import { ContactList } from './Phonebook/ContactList'
+import { ContactForm } from './Phonebook/ContactForm'
+import { Filter } from './Phonebook/Filter'
+import styles from './Phonebook/Phonebook.module.css'
+
+
+export function App(){
+  const [contacts, setContacts] = useState([])
+  const [filter, setFilter] = useState('')
+
+
+  function hendleSubmit (name, number) {
+    if (contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+      return alert(`${name} is already in contacts.`)
+    }
+    setContacts([...contacts, {name, number}])
+  }
+  
+
+  function onDelete(index) {
+    setContacts(contacts.filter((arr, idx) => idx !== index))
+  }
+
+     function hendleFilter (value) {
+        setFilter(value)
+        
+    }
+
+
+  function hendleFind() {
+    const normalizedFilter = filter.toLowerCase();
+    return (contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    ))
+    
+  }
+
+    return (
+      <div>
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm hendleSubmit={hendleSubmit} />
+        <h2 className={styles.title}>Contacts</h2>
+        <Filter filter={filter} hendleFilter={hendleFilter}/>
+        <ContactList contacts={hendleFind()} onDeleteItem = {onDelete}/>
+      </div>
+    )
+  }
